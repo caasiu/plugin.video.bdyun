613aea7ef25d305a9324be2a78721ffc226b66f5
caasiu <Rogers619952467@gmail.com>
2016-05-14 23:33:51 +0000

    make comments in chinese keyboard module

--- lib/resource/cnkeyboard.py
+++ lib/resource/cnkeyboard.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-import xbmc
+'''
+this module is for supporting chinese input
+the ChineseKeyboard is from script.module.keyboard.chinese
+if can not load chinese keyboard then use enlish keyboard
+'''
 
 try:
     import ChineseKeyboard as _xbmc
-    #kb = ChineseKeyboard.Keyboard()
 except ImportError as e:
     import xbmc as _xbmc
-    #kb = xbmc.Keyboard()
 
 
 def keyboard(default='', heading='', hidden=False):
-    #_kb.setDefault(default)
-    #_kb.setHeading(heading)
-    #_kb.setHiddenInput(hidden)
     kb = _xbmc.Keyboard(default,heading)
     kb.doModal()
     if kb.isConfirmed():
